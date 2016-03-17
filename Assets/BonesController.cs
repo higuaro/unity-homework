acfec2de020c08162735b4473babc6cc0f71d939
higuaro <henry.eduardo@gmail.com>
2016-03-17 23:48:34 +1100

    Initial commit

---
+++ Assets/BonesController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BonesController : MonoBehaviour {
+	private Rigidbody2D rigidBody2d;
+	private float vel = 1f;
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody2d = GetComponent <Rigidbody2D> ();
+		animator = GetComponent <Animator> ();
+	}
+
+	void FixedUpdate () {
+		var currentAnimStateInfo = animator.GetCurrentAnimatorStateInfo (0);
+		if (currentAnimStateInfo.IsName ("bones-standing")) { 
+			if (Random.value < 1f / (60f)) {
+				animator.SetTrigger ("walk");
+			}
+		} else if (currentAnimStateInfo.IsName("bones-walking")) {
+			// Nope!
+			Vector2 v = new Vector2 (vel, 0);
+			rigidBody2d.velocity = v;
+		}
+	}
+
+	void OnTriggerEnder2D(Collider2D other) {
+		//StartAttackingAnimation ();
+	}
+
+	void StartAttackingAnimation() {
+		animator.SetTrigger ("attack");
+	}
+
+	void Flip() {
+		vel *= -1;
+		var s = transform.localScale;
+		s.x *= -1;
+		transform.localScale = s;
+	}
+}
